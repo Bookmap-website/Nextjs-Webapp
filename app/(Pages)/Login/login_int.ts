89f4ab06d72bf49a90f16acdb59ec92e9087ddc5
Olivier <oliviernadeauallo@gmail.com>
2026-04-16 19:09:26 -0400

    asd

--- app/(Pages)/Login/login_int.ts
+++
@@ -1,4 +0,0 @@
-export default interface Login_int {
-    email: string;
-    password: string;
-}
--- app/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div>
-      <img src="/loading.gif"></img>
-    </div>
-  );
-}
